c65d98ba4727ca1afe3f77adb9d3443882e0c5a1
Brendan Mills <brendanrmills@gmail.com>
2023-04-13 15:01:38 -0700

    add local time

--- plot.py
+++ plot.py
@@ -12,10 +12,6 @@ import pytz
 import pickle
 import folium
 
-df = pd.read_pickle('events.pkl')
-print(df)
-colors = ['#808080', '#ffff66', '#ff4d4d']
-    
 def plot_plotly():
     fig = go.Figure()
     fig.add_trace(go.Scattergeo(
@@ -25,7 +21,6 @@ def plot_plotly():
         text = df['mag'],
         marker = dict(
             size = 1.25*df['mag']**2,
-    #        color = colors[i-6],
             line_width = 0
         ),
         hovertemplate =
@@ -53,6 +48,9 @@ def plot_plotly():
     fig.show()
 
 def plot_gmt():
+    '''
+    This method prints the data using pygmt
+    '''
     region = [
         df.lon.min() - 1,
         df.lon.max() + 1,
@@ -73,28 +71,58 @@ def plot_gmt():
     )
     fig.show()
 
-def plot_folium():
+def get_color(quake_time):
+    '''
+    This helper method takes a time and determines if it happens within one hour or one day and appropriately returns a color for the event
+    '''
+    td = datetime.utcnow() - quake_time
+    week = timedelta(days=7)
+    day = timedelta(days=1)
+    hour = timedelta(hours=1)
+    #determine how recently it happened
+    if td < hour:
+        c = 'red'
+    elif td < day:
+        c = 'orange'
+    else:
+        c = 'yellow'
+    return c
+    
+def localize_df(df):
+    '''
+    This method takes a dataframe and looks for a time column,
+    it if finds one it returns a copy of the df with a column
+    of the assiciated times in the pacific time zone.
+    '''
+    pacific = pytz.timezone('US/Pacific')
+    df_out = df.copy()
+    times = df['time']
+    loc_times = []
+    for t in times:
+        utc_time = pytz.utc.localize(t)
+        loc_time = utc_time.astimezone(pacific)
+        loc_times.append(loc_time)
+    df_out['loc_time'] = loc_times
+    return df_out
+
+def plot_folium(df):
     m = folium.Map(location=[34, -118],tiles="Stamen Terrain",
         width="%100",
         height="%100",
         zoom_start=6.5)
     tooltip = "Click for info!"
     
+    #Iterate over all the rows(quakes) in the dataframe
     for i, row in df.iterrows():
         #this corrects for a magnitude earthquake of 0
         if row['mag'] == 0:
             row['mag'] = 0.001
-        time = row['time'].strftime("%m/%d/%Y, %H:%M:%S")
-        td = datetime.now() - row['time']
-        c = colors[0]
-        if td < timedelta(days=1):
-            c = colors[1]
-        elif td < timedelta(hours=1):
-            c = colors[2]
-#        print(td > timedelta(days=1))
+        time = row['loc_time'].strftime("%H:%M:%S")
+        date = row['loc_time'].strftime("%m/%d/%Y")
+        c = get_color(row['time'])
         
         #set up the text to display
-        iframe = folium.IFrame('Time: ' + time + '<br>' + 'Mag: ' + str(row['mag']) + ' ' + row['type'] + '<br>' + 'Depth: ' + str(row['depth']) + f' km<br>SCEDC info <a href="https://scedc.caltech.edu/recent/Quakes/ci{row["id"]}.html" target="_blank">here</a>')
+        iframe = folium.IFrame(date + '<br>Local Time: ' + time + '<br>' + 'Mag: ' + str(row['mag']) + ' ' + row['type'] + '<br>' + 'Depth: ' + str(row['depth']) + f' km<br>SCEDC info <a href="https://scedc.caltech.edu/recent/Quakes/ci{row["id"]}.html" target="_blank">here</a>')
         
         #place the marker
         popup = folium.Popup(iframe, min_width=200, max_width=200)
@@ -105,5 +133,10 @@ def plot_folium():
             tooltip=tooltip,
             popup=popup).add_to(m)
     m.save('index.html')
+#read dataframe
+df = pd.read_pickle('events.pkl')
+#add local time column
+df = localize_df(df)
+colors = ['#808080', '#ffff66', '#ff4d4d']
 
-plot_folium()
+plot_folium(df)
